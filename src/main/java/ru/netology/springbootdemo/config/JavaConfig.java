f7cf1cd2f7d7333ee4c931fd93e67350aa2c54de
Azerty <danila.94@mail.ru>
2024-03-29 00:17:33 +0300

    2

--- src/main/java/ru/netology/springbootdemo/config/JavaConfig.java
+++ src/main/java/ru/netology/springbootdemo/config/JavaConfig.java
@@ -1,22 +1,24 @@
+package ru.netology.springbootdemo.config;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.netology.springbootdemo.profiles.DevProfile;
 import ru.netology.springbootdemo.profiles.ProductionProfile;
 import ru.netology.springbootdemo.profiles.SystemProfile;
 
-@Bean(name="profile")
-@ConditionalOnProperty(name="profile", havingValue="true")
-public SystemProfile devProfile() {
-    return new DevProfile();
-}
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
 
-@Bean(name="profile")
-@ConditionalOnProperty(name="profile", havingValue="false")
-public SystemProfile prodProfile() {
-    return new ProductionProfile();
-}
-
-//@ConfigurationProperties("profile")
-public void main() {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
 }
