3ba3576ba35b80b6898ad757cb0674e6d012a254
Azerty <danila.94@mail.ru>
2024-06-29 18:16:46 +0300

    tests

--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.netology.springbootdemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +23,18 @@ class SpringBootDemoApplicationTests {
     }
     @Test
     void contextLoads() {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8080), String.class);
-        System.out.println(forEntity.getBody());
-        myapp.getMappedPort(8081);
-        myapp2.getMappedPort(8082);
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:" +
+                myapp.getMappedPort(8081) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is production\n", entity.getBody());
+        System.out.println(entity.getBody());
+    }
+
+    @Test
+    void contextLoads2() {
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:" +
+                myapp2.getMappedPort(8082) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is dev\n", entity.getBody());
+        System.out.println(entity.getBody());
+
     }
 }
